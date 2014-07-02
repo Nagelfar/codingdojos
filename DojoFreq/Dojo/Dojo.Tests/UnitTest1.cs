4437ea139a12681bb26fad345356dd6e41c2005f
Christian Folie <christian.folie@gmail.com>
2014-07-02 17:34:56 +0200

    running tests

---
+++ DojoFreq/Dojo/Dojo.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using NUnit.Framework;
+
+namespace Dojo.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            var c = new Class1();
+            Assert.IsTrue(c != null);
+        }
+    }
+}
