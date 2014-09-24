c7a117128581eeef3db1f8d11b63cae1c20be843
Christian Folie <christian.folie@gmail.com>
2014-09-24 19:38:30 +0200

    empty space

---
+++ DojoFreq/GameOfLifeTimer/GameOfLifeImpl/Space.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLifeImpl
+{
+    public class Space
+    {
+        public bool IsEmpty { get { return true; } }
+    }
+}
---
+++ DojoFreq/GameOfLifeTimer/GameOfLifeTimer/SpaceTests.cs
@@ -0,0 +1,22 @@
+﻿using GameOfLifeImpl;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLifeTimer
+{
+    [TestClass]
+    public class SpaceTests
+    {
+        [TestMethod]
+        public void EmptySpaceIsEmpty()
+        {
+            var space = new Space();
+
+            Assert.IsTrue(space.IsEmpty);
+        }
+    }
+}
