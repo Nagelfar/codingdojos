5496be45527ab142c44f37de9ffc76ead76638ca
Christian Folie <christian.folie@gmail.com>
2016-07-27 09:04:17 +0200

    TicketMachine

---
+++ Theater/test/Ticket.Test/Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TicketMachine;
+using Xunit;
+
+namespace Tests
+{
+    public class Tests
+    {
+
+        [Fact]
+        public void Test1() 
+        {
+            var t = new TicketMachine.TicketMachine();
+            
+            Assert.True(false);
+        }
+    }
+}
